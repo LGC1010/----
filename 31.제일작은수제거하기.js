7c1860427a6d16af22c7ee552399370890ab79d5
limgi <dntksql1010@naver.com>
2024-09-13 10:03:27 +0900

    제일작은수제거하기

---
+++ 31.제일작은수제거하기.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    const min = Math.min(...arr);
+
+    if (arr.length === 1) {
+            return [-1];
+    }
+    return arr.filter(x => x !== min)
+
+}
+
+console.log(solution([10]))
